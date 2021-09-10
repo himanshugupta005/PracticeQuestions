4c4fb501a7b57175e12c9ed9e295aa235bf46932
himanshugupta005 <80818447+himanshugupta005@users.noreply.github.com>
2021-09-10 18:12:47 +0530

    Add files via upload

---
+++ invert_dict.py
@@ -0,0 +1,6 @@
+my_dict={'a':1,'b':2,'c':3}
+print("original dictionary is:")
+print(my_dict)
+new_dict=dict([value,key) for key,value in my_dict.items()])
+print("dictionary after swapping is:")
+print("keys:values")
